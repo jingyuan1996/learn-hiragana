129bf5bc5e6b2f018f8689bc309ad45b696e3938
Chew Jingyuan <chew.jingyuan1996@gmail.com>
2024-07-19 00:05:33 +0800

    write function

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,51 @@ import { wordlist } from "./data/jpword.js";
 
 function App() {
   const [count, setCount] = useState(0);
+  let worlddata = wordlist[count];
 
-  function handleClick() {
+  function nextClick() {
     setCount(count + 1);
+    //console.log("test");
+  }
+
+  function minusClick() {
+    setCount(count - 1);
+    //console.log("test");
+  }
+
+  function Navgatorbutton() {
+    if (count > 0 && count < 45) {
+      //console.log(">0");
+      return (
+        <>
+          <button onClick={minusClick}>back</button>
+          <button onClick={nextClick}>next</button>
+        </>
+      );
+    } else if (count >= 45) {
+      //console.log(">13");
+      return (
+        <>
+          <button onClick={minusClick}>back</button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <button onClick={nextClick}>next</button>
+        </>
+      );
+    }
   }
 
   return (
     <>
       <div className="test">
         <center>
-          <h1>あ</h1>
-          <h2>a</h2>
+          <h1>{worlddata.jp_word}</h1>
+          <h2>{worlddata.pinyin}</h2>
           <h2>{count}</h2>
-          <button className="btn btn-primary" onClick={handleClick}>
-            test
-          </button>
+          <Navgatorbutton />
         </center>
       </div>
     </>
